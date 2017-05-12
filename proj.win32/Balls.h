fd63369d60ec042cc577b89582752f0e93712d5f
YGrylls <910357107@qq.com>
2017-05-12 14:42:49 +0800

    Add ball texture,add a dynamic element that follow user's mouse in the background of MainMenu

---
+++ proj.win32/Balls.h
@@ -0,0 +1,50 @@
+//This is the character
+//default designed size is  480*480 p
+//default designed radius=339 p
+#include <cocos2d.h>
+class Balls : public cocos2d::Sprite
+{
+private:
+	unsigned int _level;
+	int _radius;
+	int _identity;                                                     //sync with the id of Character
+public:
+	virtual void useSkill();                                      //skill to be done
+	static Balls* createWithFileName(const std::string & filename)
+	{
+		Balls *ball = new(std::nothrow) Balls;
+		if (ball&&ball->initWithFile(filename) && ball->initStatus())
+		{
+			ball->autorelease();
+			return ball;
+		}
+		CC_SAFE_DELETE(ball);
+		return nullptr;
+	}
+	bool initStatus()
+	{
+		_level = 1;
+		updateRadius();
+	}
+	void setID(int identity)
+	{
+		_identity = identity;
+	}
+	void updateRadius()                                          //update the radius and size when level changes
+	{
+		this->setScale(sqrt(_level) / 10);
+		_radius = sqrt(_level) * 339 / 10;
+	}
+	void addLevel(const int delLevel)                              //when eating other balls,call this
+	{
+		_level += delLevel;
+		if (_level > 100) _level = 100;
+	}
+	void division(const cocos2d::Vec2 & vec)
+	{
+
+		_level = _level / 2;
+		updateRadius();
+	}
+};
+
---
+++ proj.win32/Character.h
@@ -0,0 +1,7 @@
+//This is a container containing balls, which is mainly used to set the camera
+#include<cocos2d.h>
+#include "Balls.h"
+class Character :public cocos2d::Node
+{
+
+};
